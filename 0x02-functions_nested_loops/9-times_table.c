cbd74b1f7e6f3c9d808af7c976b0569661016f90
a-abdelmageed <a.abdelmajeedali@gmail.com>
2023-03-22 19:25:25 -0700

    Task: 9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,14 +7,14 @@
  */
 void times_table(void)
 {
-	int a, b, op;
+	int a, c, op;
 
 	for (a = 0; a <= 9; a++)
 	{
 		_putchar(48);
-		for (b = 1; b <= 9; b++)
+		for (c = 1; c <= 9; c++)
 		{
-			op = a * b;
+			op = a * c;
 			_putchar(44);
 			_putchar(32);
 			if (op <= 9)
