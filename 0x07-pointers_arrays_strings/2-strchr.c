7169caebfa81686f50b7b31e395e9860bfd41f18
a-abdelmageed <a.abdelmajeedali@gmail.com>
2023-04-03 18:52:12 -0700

    Task: 2. strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,6 +3,8 @@
  * _strchr - locates a character in a string,
  * Return: the pointer to the first occurrence of the character c.
  */
+char *_strchr(char *s, char c);
+
 char *_strchr(char *s, char c)
 {
 	unsigned int i = 0;
