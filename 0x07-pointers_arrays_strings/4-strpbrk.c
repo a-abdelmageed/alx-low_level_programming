9a977f39cff9084def8ea3b5316009b37a2a8a4f
a-abdelmageed <a.abdelmajeedali@gmail.com>
2023-04-03 19:28:07 -0700

    Task: 4. strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,19 +2,27 @@
 
 /**
  * _strpbrk - searches a string for any of a set of bytes.
- * Return: a pointer to the byte in s that matches one of the
+ * @s: string to be scanned
+ * @accept: string containing characters to match
+ * Description: This function finds the first character in the string s
+ * that matches any character specified in accept.
+ * This does not include terminating null-characters.
+ * Return: pointer to the byte in s that matches one of the bytes in accept
  */
 char *_strpbrk(char *s, char *accept)
 {
-	unsigned int i, j;
+	int i, j;
 
-	for (i = 0; *(s + i) != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; *(accept + j) != '\0'; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (*(s + i) == *(accept + j))
+			if (s[i] == accept[j])
+			{
 				return (s + i);
+			}
 		}
 	}
-	return ('\0');
+
+	return (0);
 }
